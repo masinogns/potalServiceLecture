4137c8a6bbdf3425201e1a1e9c347b7eff9e179b
b.kihoon <masinogns@gmail.com>
2017-03-15 21:25:58 +0900

    add() method 추가

--- potalServiceLecture/src/UserDao.java
+++ potalServiceLecture/src/UserDao.java
@@ -8,12 +8,11 @@ import static java.lang.Class.forName;
 public class UserDao {
     public User get(Long id) throws ClassNotFoundException, SQLException {
 
+        Connection connection = getConnection();
         // User 어디에있어? Mysql
         // Class 를 로딩해야겠네
-        Class.forName("com.mysql.jdbc.Driver");
-        // 커넥션을 맺어야겠네
-        Connection connection = DriverManager.getConnection("jdbc:mysql://113.198.162.186/test","root","as0109247");
-        // 쿼리를 만들어야겠네
+        // class가 로드될 때 끌고와서 맺어주는 거다.
+
         PreparedStatement preparedStatement = connection.prepareStatement("SELECT * from user where id = ?");
         preparedStatement.setLong(1,id);
         // 쿼리를 실행해야겠네
@@ -31,5 +30,38 @@ public class UserDao {
         // 결과를 리턴해야겠네
 
         return user;
+    }
+
+    public Long add(User user) throws ClassNotFoundException, SQLException {
+
+        Connection connection = getConnection();
+
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT into user(name, password) VALUES (?,?)");
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.executeUpdate();
+
+        preparedStatement = connection.prepareStatement("SELECT last_insert_id()");
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        Long id = resultSet.getLong(1);
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+
+        return id;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        // 커넥션을 맺어야겠네
+        Connection connection = DriverManager.getConnection("jdbc:mysql://113.198.162.186/test?characterEncoding=utf-8","root","as0109247");
+        // 쿼리를 만들어야겠네
+
+        return connection;
     }
 }
--- potalServiceLecture/test/UserDaoTest.java
+++ potalServiceLecture/test/UserDaoTest.java
@@ -20,9 +20,29 @@ public class UserDaoTest {
         UserDao userDao = new UserDao();
         User user = userDao.get(id);
 
+        // asserThat아 id랑 가져온 아이디랑 맞는지 알려줘
         assertThat(id, is(user.getId()));
         assertThat(name, is(user.getName()));
         assertThat(password, is(user.getPassword()));
 
+    }
+
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        User user = new User();
+
+        String name = "kihoon";
+        String password = "0101010";
+
+        user.setName(name);
+        user.setPassword(password);
+
+        UserDao userDao = new UserDao();
+        Long id = userDao.add(user);
+        User resultUser = userDao.get(id);
+
+        assertThat(id, is(resultUser.getId()));
+        assertThat(name, is(resultUser.getName()));
+        assertThat(password, is(resultUser.getPassword()));
     }
 }
